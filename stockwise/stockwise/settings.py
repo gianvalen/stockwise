752f09ed5925317b8f2502310227fa966ce92ee7
gianvalen <157687026+gianvalen@users.noreply.github.com>
2025-05-15 08:57:44 +0800

    Added new apps

--- stockwise/stockwise/settings.py
+++ stockwise/stockwise/settings.py
@@ -40,6 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'stock_management',
+    'user_management',
+    'procurement',
+    'project_management',
+    'supply',
 ]
 
 MIDDLEWARE = [
@@ -57,7 +61,9 @@ ROOT_URLCONF = 'stockwise.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
