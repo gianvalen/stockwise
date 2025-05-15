9b12e627b9aec5c19e1db0cd38f7acd6dcf4d86e
Jade <jade.ramos@student.ateneo.edu>
2025-05-15 16:22:17 +0800

    Added offer view for supplier

--- stockwise/supply/views.py
+++ stockwise/supply/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from user_management.decorators import user_type_required
-from stock_management.models import PurchaseRequest, RequestDetail, Project
+from stock_management.models import PurchaseRequest, RequestDetail
+from .forms import OfferForm
 
 @login_required
 @user_type_required('supplier')
@@ -28,3 +30,40 @@ def requests_detail(request, pr_id):
         'request_details': request_details
     }
     return render(request, 'requests_detail.html', context)
+
+@login_required
+@user_type_required('supplier')
+def offer_material(request, pr_id, material_id):
+    purchase_request = get_object_or_404(PurchaseRequest, pr_id=pr_id)
+    request_detail = get_object_or_404(RequestDetail, pr=purchase_request, material__material_id=material_id)
+
+    if request.method == 'POST':
+        form = OfferForm(request.POST)
+        if form.is_valid():
+            offer = form.save(commit=False)
+            offer.offer_id = generate_offer_id()
+            offer.offer_date = timezone.now()
+            offer.offer_status = "Pending"
+            offer.save()
+
+            return redirect('supply:requests_detail', pr_id=pr_id)
+    else:
+        form = OfferForm()
+
+    context = {
+        'form': form,
+        'material': request_detail.material,
+        'purchase_request': purchase_request,
+    }
+
+    return render(request, 'offer_material.html', context)
+
+import random
+import string
+from stock_management.models import Offer
+
+def generate_offer_id():
+    while True:
+        offer_id = ''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
+        if not Offer.objects.filter(offer_id=offer_id).exists():
+            return offer_id
