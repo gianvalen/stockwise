0862d0842075930ce0e2552328052717a78e263f
Jade <jade.ramos@student.ateneo.edu>
2025-05-15 11:05:36 +0800

    Modified settings to include new template folders and new URLs

--- stockwise/user_management/models.py
+++ stockwise/user_management/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    USER_TYPE_CHOICES = (
+        ('manager', 'Project Manager'),
+        ('warehouse', 'Warehouse Staff'),
+        ('procurement', 'Procurement Officer'),
+        ('supplier', 'Supplier'),
+    )
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    user_type = models.CharField(max_length=20, choices=USER_TYPE_CHOICES)
+    image = models.ImageField(upload_to='profile_pics/', default='default.jpg', blank=True)
+
+    def get_user_type_display(self):
+        return dict(self.USER_TYPE_CHOICES).get(self.user_type, self.user_type)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.get_user_type_display()}"
--- stockwise/user_management/views.py
+++ stockwise/user_management/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth import login
+from .forms import UserRegistrationForm
 
 # Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+    else:
+        form = UserRegistrationForm()
+
+    return render(request, 'register.html', {'form': form})
