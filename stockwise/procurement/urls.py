c5b3e743287d3c793fdad599d215d404419ac1fd
gianvalen <157687026+gianvalen@users.noreply.github.com>
2025-05-16 14:01:10 +0800

    Added my requests for proc and manager

--- stockwise/procurement/urls.py
+++ stockwise/procurement/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home_procurement, projects_list, project_detail, request_material, pending_offers
+from .views import home_procurement, projects_list, project_detail, request_material, pending_offers, my_requests
 
 app_name = 'procurement'
 
@@ -8,7 +8,8 @@ urlpatterns = [
     path('projects/', projects_list, name='projects_list'),
     path('projects/<str:project_id>/', project_detail, name='project_detail'),
     path('projects/<str:project_id>/request-material/', request_material, name='purchase_request_create'),
-    path('pending-offers/', pending_offers, name='pending_offers')
+    path('pending-offers/', pending_offers, name='pending_offers'),
+    path('requests/', my_requests, name='my_requests'),
 
 
 ]
