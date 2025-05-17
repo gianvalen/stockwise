78bac059194a7790771cf1a910d3303e065904ab
gianvalen <157687026+gianvalen@users.noreply.github.com>
2025-05-17 15:37:17 +0800

    Fixed usertype decorator

--- stockwise/user_management/decorators.py
+++ stockwise/user_management/decorators.py
@@ -2,18 +2,18 @@ from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
 from functools import wraps
 
-def user_type_required(required_user_type):
+def user_type_required(*allowed_user_types):
     def decorator(view_func):
         @wraps(view_func)
         @login_required
         def wrapper(request, *args, **kwargs):
             user_profile = getattr(request.user, 'profile', None)
-            if user_profile and user_profile.user_type == required_user_type:
+            actual_user_type = user_profile.user_type if user_profile else None
+
+            if actual_user_type in allowed_user_types:
                 return view_func(request, *args, **kwargs)
             else:
                 # Redirect to the appropriate dashboard based on actual user_type
-                actual_user_type = user_profile.user_type if user_profile else None
-
                 if actual_user_type == 'manager':
                     return redirect('project_management:home_manager')
                 elif actual_user_type == 'warehouse':
@@ -23,8 +23,7 @@ def user_type_required(required_user_type):
                 elif actual_user_type == 'supplier':
                     return redirect('supply:home_supplier')
                 else:
-                    # Fallback redirect (e.g., login page)
                     return redirect('user_management:login')
-
         return wrapper
     return decorator
+
